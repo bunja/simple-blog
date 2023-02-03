2ece79da8cda5cd3b770dde7a189b594e1a97021
Larisa <kos.lv@yandex.com>
2023-02-03 13:33:26 +0100

    templates

--- blogpost/posts/views.py
+++ blogpost/posts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-from .models import Post, Category, Autho
+from .models import Post, Category, Author
+
+def get_author(user):
+    qs = Author.objects.filter(user=user)
+    if qs.exists():
+        return qs[0]
+    return None
 
 def homepage(request):
     categories = Category.objects.all()[0:3]
@@ -11,3 +17,51 @@ def homepage(request):
         'categories':categories,
     }
     return render(request, 'homepage.html', context)
+
+def post(request,slug):
+    post = Post.objects.get(slug=slug)
+    context = {
+        'post': post,
+    }
+    return render(request, 'post.html', context)
+
+def about (request):
+    return render(request, 'about_page.html')
+
+def search(request):
+    queryset = Post.objects.all()
+    query = request.GET.get('q')
+    if query:
+        queryset = queryset.filter(
+            Q(title__icontains=query) |
+            Q(overview__icontains=query)
+        ).distinct()
+    context = {
+        'object_list': queryset
+    }
+    return render(request, 'search_bar.html', context)
+
+def postlist (request,slug):
+    category = Category.objects.get(slug = slug)
+    posts = Post.objects.filter(categories__in=[category])
+
+    context = {
+        'posts': posts,
+        'category': category,
+    }
+    return render(request, 'post_list.html', context)
+
+def category_post_list (request, slug):
+    category = Category.objects.get(slug = slug)
+    posts = Post.objects.filter(categories__in=[category])
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'post_list.html', context)
+
+def allposts(request):
+    posts = Post.objects.order_by('-timestamp')
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'all_posts.html', context)
